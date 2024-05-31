8e84539652c6370c75aca6e2d5bfca331ad30b1e
CharLemAznable <545541819@qq.com>
2024-06-01 04:01:57 +0800

    Fixed code smell.

--- src/test/java/com/github/charlemaznable/logback/dendrobe/etcd/EsAppenderTest.java
+++ src/test/java/com/github/charlemaznable/logback/dendrobe/etcd/EsAppenderTest.java
@@ -68,10 +68,8 @@ public class EsAppenderTest implements EtcdUpdaterListener, EsClientManagerListe
         esConfig.setPassword(ELASTICSEARCH_PASSWORD);
         esClient = buildElasticsearchClient(esConfig);
 
-        val createIndexRequest = CreateIndexRequest.of(builder -> builder.index("logback.etcd"));
-        val createIndexResponse = esClient.indices().create(createIndexRequest);
-        val openIndexRequest = OpenRequest.of(builder -> builder.index("logback.etcd"));
-        val openIndexResponse = esClient.indices().open(openIndexRequest);
+        esClient.indices().create(CreateIndexRequest.of(builder -> builder.index("logback.etcd")));
+        esClient.indices().open(OpenRequest.of(builder -> builder.index("logback.etcd")));
 
         root = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
         self = LoggerFactory.getLogger(EsAppenderTest.class);
